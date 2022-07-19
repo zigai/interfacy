8c6dfd8905199723e3bd0fb49033ed27f66e5d7b
zigai <ziga.ivansek@gmail.com>
2022-07-19 11:19:28 +0200

    rename core files

---
+++ interfacy/interfacy_class.py
@@ -0,0 +1,23 @@
+import inspect
+
+from interfacy.interfacy_function import InterfacyFunction
+
+
+class InterfacyClass:
+
+    def __init__(self, cls) -> None:
+        self.cls = cls
+        self.name: str = self.cls.__name__
+        self.docstr = inspect.getdoc(self.cls)
+        if self.docstr is None:
+            self.docstr = ""
+        members = inspect.getmembers(self.cls, inspect.isfunction)
+        self.has_init = members[0][0] == "__init__"
+        self.methods = [InterfacyFunction(i[1]) for i in members]
+
+    @property
+    def has_docstr(self):
+        return len(self.docstr) != 0
+
+    def __repr__(self) -> str:
+        return f"InterfacyClass(name={self.name}, methods={len(self.methods)}, has_init={self.has_init}, has_docstr={self.has_docstr})"
---
+++ interfacy/interfacy_function.py
@@ -0,0 +1,22 @@
+import inspect
+
+from interfacy.interfacy_parameter import InterfacyParameter
+
+
+class InterfacyFunction:
+
+    def __init__(self, func) -> None:
+        self.func = func
+        self.name: str = self.func.__name__
+        self.docstr = inspect.getdoc(self.func)
+        if self.docstr is None:
+            self.docstr = ""
+        func_args = inspect.signature(self.func)
+        self.parameters = [InterfacyParameter(i) for i in func_args.parameters.values()]
+
+    @property
+    def has_docstr(self):
+        return len(self.docstr) != 0
+
+    def __repr__(self) -> str:
+        return f"InterfacyFunction(name={self.name}, parameters={len(self.parameters)}, has_docstr={self.has_docstr})"
