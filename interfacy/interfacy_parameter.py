25a15828b7631b4f784d492498348a5c5186d5b9
zigai <ziga.ivansek@gmail.com>
2022-07-28 20:13:46 +0200

    InterfacyParameter refactor

--- interfacy/interfacy_parameter.py
+++ interfacy/interfacy_parameter.py
@@ -1,19 +1,43 @@
+import enum
 import inspect
+import types
+import typing
 
-from stdl.str_util import ColorANSI, str_with_color
+from stdl.str_util import Color, str_with_color
 
+from interfacy.cli_parsers import CLI_PARSER
 from interfacy.util import type_as_str
 
 EMPTY = inspect._empty
-DEFAULT_CLI_THEME = {"type": ColorANSI.LIGHT_YELLOW, "value": ColorANSI.LIGHT_BLUE}
+SIMPLE_TYPES = [str, int, float, bool]
+
+SpecialGenericAlias = typing._SpecialGenericAlias
+UnionGenericAlias = typing._UnionGenericAlias
+from typing import Any
+
+
+class ParameterKind(enum.Enum):
+    BASIC = 1
+    UNSUPPORTED = 2
+    UNION = 3
+    ALIAS = 4
+    SPECIAL = 5
 
 
 class InterfacyParameter:
+    DEFAULT_CLI_THEME = {"type": Color.LIGHT_YELLOW, "default": Color.LIGHT_BLUE}
 
-    def __init__(self, param: inspect.Parameter) -> None:
-        self.type = param.annotation
-        self.name = param.name
-        self.default = param.default
+    def __init__(
+        self,
+        name: str,
+        type: Any = EMPTY,
+        default: Any = EMPTY,
+        description: str | None = None,
+    ) -> None:
+        self.name = name
+        self.type = type
+        self.default = default
+        self.description = description
 
     @property
     def dict(self):
@@ -21,10 +45,26 @@ class InterfacyParameter:
             "name": self.name,
             "type": self.type,
             "default": self.default,
+            "description": self.description,
         }
 
     def __repr__(self):
-        return f"InterfacyParameter(type={self.type}, name={self.name}, default={self.default})"
+        data = f"name={self.name}, type={self.type}, default={self.default}, description={self.description}"
+        return f"Parameter({data})"
+
+    @property
+    def kind(self) -> ParameterKind:
+        if self.type == EMPTY:
+            return ParameterKind.BASIC
+        if self.type in SIMPLE_TYPES:
+            return ParameterKind.BASIC
+        if self.type in CLI_PARSER.keys():
+            return ParameterKind.SPECIAL
+        if type(self.type) in [types.UnionType, UnionGenericAlias]:
+            return ParameterKind.UNION
+        if type(self.type) in [types.GenericAlias, SpecialGenericAlias]:
+            return ParameterKind.ALIAS
+        return ParameterKind.UNSUPPORTED
 
     @property
     def is_typed(self) -> bool:
@@ -35,16 +75,40 @@ class InterfacyParameter:
         return self.default == EMPTY
 
     @property
+    def is_optional(self) -> bool:
+        return not self.is_required
+
+    @property
     def flag_name(self) -> str:
         return f"--{self.name}"
 
-    def get_help_str(self, theme) -> str:
+    def help_string(self, theme=DEFAULT_CLI_THEME) -> str:
         if self.is_required and not self.is_typed:
             return ""
         help_str = []
+
         if self.is_typed:
-            help_str.append(str_with_color(type_as_str(self.type), theme['type']))
-        if not self.is_required:
-            help_str.append(f"default: {str_with_color(self.default, theme['value'])}")
+            if theme is not None:
+                help_str.append(str_with_color(type_as_str(self.type), theme['type']))
+            else:
+                help_str.append(type_as_str(self.type))
+
+        if self.is_optional:
+            if theme is not None:
+                help_str.append(f"default: {str_with_color(self.default, theme['default'])}")
+            else:
+                help_str.append(f"default: {self.default}")
+
         help_str = ", ".join(help_str)
+        if self.description is not None:
+            help_str = f"{self.description} [{help_str}]"
         return help_str
+
+    @classmethod
+    def from_inspect_param(cls, param: inspect.Parameter, description: str | None = None):
+        return cls(
+            name=param.name,
+            type=param.annotation,
+            default=param.default,
+            description=description,
+        )
