8247c0764c7eb4d14d6ec786a6001a61a2071891
zigai <ziga.ivansek@gmail.com>
2022-07-29 23:34:41 +0200

    casting utils

--- interfacy/util.py
+++ interfacy/util.py
@@ -10,3 +10,41 @@ def type_as_str(t):
 
 def is_file(s):
     return os.path.isfile(s)
+
+
+def cast_to(t):
+    """
+    Returns a functions that casts a string to type 't'
+    """
+
+    def inner(arg: str) -> t:
+        if isinstance(arg, t):
+            return arg
+        return t(arg)
+
+    return inner
+
+
+def cast_iter_to(iterable, t):
+
+    def inner(arg) -> iterable[t]:
+        l = [t(i) for i in arg]
+        return iterable(l)
+
+    return inner
+
+
+def cast_dict_to(k, v):
+
+    def inner(arg: dict) -> dict[k, v]:
+        return {k(key): v(val) for key, val in arg.items()}
+
+    return inner
+
+
+def parse_and_cast(parser, caster):
+
+    def inner(val: str):
+        return caster(parser(val))
+
+    return inner
