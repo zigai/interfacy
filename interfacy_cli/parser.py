dfb1bbda8a57ee20d90b83ab6152a559049047b4
zigai <ziga.ivansek@gmail.com>
2023-04-18 22:38:02 +0200

    refactor InterfacyArgumentParser

--- interfacy_cli/parser.py
+++ interfacy_cli/parser.py
@@ -1,30 +1,30 @@
-from argparse import Action, FileType
-from typing import Any, Callable, Iterable, Type
+import sys
+from functools import partial
 
+import strto
 from nested_argparse import NestedArgumentParser
-from strto import get_parser
 
-from interfacy_cli.safe_help_formatter import SafeRawHelpFormatter
+from interfacy_cli.argparse_wrappers import SafeRawHelpFormatter, _ArgumentParser
+from interfacy_cli.util import get_args
 
 
 class InterfacyArgumentParser:
-    formater_class = SafeRawHelpFormatter
-    parser_class = NestedArgumentParser
-
     def __init__(
         self,
         desciption: str | None = None,
         epilog: str | None = None,
         from_file_prefix: str = "@F",
         allow_args_from_file: bool = True,
-        install_tab_completion: bool = False,
+        type_parser: strto.Parser | None = None,
+        formatter_class=SafeRawHelpFormatter,
     ) -> None:
         self.from_file_prefix = from_file_prefix
         self.allow_args_from_file = allow_args_from_file
-        self.install_tab_completion = install_tab_completion
+        self.type_parser = type_parser or strto.get_parser()
+        self.formatter_class = formatter_class
 
-        self._parser = self.parser_class(
-            formatter_class=self.formater_class,
+        self._parser: NestedArgumentParser = _ArgumentParser(
+            formatter_class=self.formatter_class,
             description=desciption,
             epilog=epilog,
         )
@@ -38,7 +38,7 @@ class InterfacyArgumentParser:
         return self._parser.epilog
 
     @property
-    def parser(self):
+    def parser(self) -> NestedArgumentParser:
         return self._parser
 
     def set_description(self, description: str | None = None) -> None:
@@ -47,39 +47,24 @@ class InterfacyArgumentParser:
     def set_epilog(self, epilog: str | None = None) -> None:
         self._parser.epilog = epilog
 
-    def add_argument(
-        self,
-        *name_or_flags: str,
-        action: str | Type[Action] | None = ...,
-        nargs: int | str | None = ...,
-        default: Any = ...,
-        type: Any = ...,
-        choices: Iterable | None = None,
-        required: bool = False,
-        help: str | None = None,
-        const: Any = ...,
-        metavar: str | tuple[str, ...] | None = None,
-        dest: str | None = None
-    ) -> None:
-        # TODO: type
-        self._parser.add_argument(
-            *name_or_flags,
-            action=action,  # type:ignore
-            nargs=nargs,  # type:ignore
-            default=default,
-            choices=choices,
-            required=required,
-            help=help,
-            const=const,
-            metavar=metavar,
-            dest=dest,
-        )
+    def add_argument(self, *args, **kwargs):
+        if "type" in kwargs:
+            func = partial(self.type_parser.parse, t=kwargs["type"])
+            kwargs["type"] = func
+        return self._parser.add_argument(*args, **kwargs)
 
-    def parse_args(self):
-        ...
+    def parse_args(self, args: list[str] | None = None):
+        if args is None:
+            args = self.get_args()
+        return self._parser.parse_args(args)
 
-    def add_subparsers(self):
-        ...
+    def add_subparsers(self, *args, **kwargs):
+        return self._parser.add_subparsers(*args, **kwargs)
 
-    def get_args(self):
-        ...
+    def get_args(self) -> list[str]:
+        if self.allow_args_from_file:
+            return get_args(sys.argv, from_file_prefix=self.from_file_prefix)
+        return sys.argv[1:]
+
+
+__all__ = ["InterfacyArgumentParser"]
