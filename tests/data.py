e08705f0272145467bed93ad0a87901cef7b6efc
zigai <ziga.ivansek@gmail.com>
2023-09-16 23:03:34 +0200

    tests

---
+++ tests/data.py
@@ -0,0 +1,68 @@
+def pow(base: int, exponent: int = 2) -> int:
+    """
+    Raise base to the power of exponent.
+
+    Args:
+        base (int): The base number.
+        exponent (int, optional): The power to which the base is raised.
+
+    Returns:
+        int: Result of base raised to exponent.
+    """
+    return base**exponent
+
+
+class Math:
+    """
+    A simple math class.
+
+    Args:
+        rounding (int, optional): The number of decimal places to round to.
+
+    """
+
+    def __init__(self, rounding: int = 6) -> None:
+        self.rounding = rounding
+
+    def _round(self, value: float | int) -> float | int:
+        return round(value, self.rounding)
+
+    def pow(self, base: int, exponent: int = 2) -> float:
+        """
+        Raise base to the power of exponent.
+
+        Args:
+            base (int): The base number.
+            exponent (int, optional): The power to which the base is raised.
+
+        Returns:
+            float: Result of base raised to exponent.
+        """
+        return self._round(base**exponent)
+
+    def add(self, a: int, b: int) -> float:
+        """
+        Add two numbers.
+
+        Args:
+            a (int): First number.
+            b (int): Second number.
+
+        Returns:
+            float: Sum of a and b.
+        """
+
+        return self._round(a + b)
+
+    def subtract(self, a: int, b: int) -> float:
+        """
+        Subtract two numbers.
+
+        Args:
+            a (int): First number.
+            b (int): Second number.
+
+        Returns:
+            float: Difference of a and b.
+        """
+        return self._round(a - b)
---
+++ tests/test_auto_argument_parser.py
@@ -0,0 +1,36 @@
+import objinspect
+from data import Math, pow
+
+from interfacy_cli.parser import AutoArgparseParser
+
+
+def test_from_function():
+    func = objinspect.Function(pow)
+    auto_parser = AutoArgparseParser(desciption=func.description)
+    parser = auto_parser.parser_from_func(func)
+    assert parser.description == func.description
+
+    args = parser.parse_args(["2", "-e", "2"])
+    assert args.base == "2"
+    assert args.exponent == "2"
+
+    args = parser.parse_args(["2"])
+    assert args.base == "2"
+    assert args.exponent == 2
+
+
+def test_from_method():
+    math = Math()
+    method = objinspect.objinspect(math.pow)
+    assert isinstance(method, objinspect.Method)
+    auto_parser = AutoArgparseParser(desciption=method.description)
+    parser = auto_parser.parser_from_method(method)
+    assert parser.description == method.description
+
+    args = parser.parse_args(["2", "-e", "2"])
+    assert args.base == "2"
+    assert args.exponent == "2"
+
+    args = parser.parse_args(["2"])
+    assert args.base == "2"
+    assert args.exponent == 2
