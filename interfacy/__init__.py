b512ac768aaa578b4e64ce645151f5b021a5fdb4
zigai <ziga.ivansek@gmail.com>
2026-05-28 08:23:56 +0200

    style: remove unnecessary __dir__ from package init

--- interfacy/__init__.py
+++ interfacy/__init__.py
@@ -4,9 +4,4 @@ from interfacy.executable_flag import ExecutableFlag
 from interfacy.group import CommandGroup
 from interfacy.interfacy import Interfacy
 
-
-def __dir__() -> list[str]:
-    return sorted(__all__)
-
-
 __all__ = ["CommandGroup", "ExecutableFlag", "Interfacy"]
